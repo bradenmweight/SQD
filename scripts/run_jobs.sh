5e1a7eb72c87df2c9ea2d0038eed373923fbc7a5
bradenmweight <braden.m.weight@gmail.com>
2023-01-15 17:44:52 -0500

    Make HTML branch

--- scripts/run_jobs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for n in {5..199}; do 
-    cp NAMD.in TRAJ/traj-${n}/; 
-    cp submit.MMMM_NAMD TRAJ/traj-${n}/; 
-    cd TRAJ/traj-${n}/; 
-        sbatch submit.MMMM_NAMD ; 
-        cd ../../; done
